e544745b49b517c80c6e83584f85fb9ba4d6a3b1
Your Name <jakkula4@gmail.com>
2012-06-05 11:34:19 +0530

    tapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mk::Application.config.secret_token = '69174e42339033eb1f8e094836296a4ebb760ba173bf7972f1bec13f14fc1aa9b68fee1804e3831ce9ff55ec57c7bcbd3945f163ec27bc5a138b6c7e0a3bce4c'
